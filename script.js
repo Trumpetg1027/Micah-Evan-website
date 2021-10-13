75cb2fd1506656993fedd5fedfe573d30103cad7
Trumpetg1027 <92411161+Trumpetg1027@users.noreply.github.com>
2021-10-12 19:36:28 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+
+let aboutMe = "Hi there!  I'm getting into web development!";
+document.getElementById('aboutText').innerText = aboutMe;
+let aboutLink = document.createElement('div');
+document.getElementById('aboutText').appendChild(aboutLink);
+
+*/
